f362dce8fb5804e0abe6c9dec4612f86168af5ca
Himanshu Maurya <mauryahimanshu567@gmail.com>
2024-04-12 10:18:33 +0530

    configure Tailwind

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
-
 import './App.css';
-
 function App() {
   return (
     <>
-    
+     <h1 className='bg-cyan-400 flex text-4xl align-middle justify-center'>Start Your Project</h1>
     </>
   );
 }
